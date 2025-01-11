f40ebf59cf34703bd76a4ca7527d151f4d348e1a
SAJEEV73 <116879542+SAJEEV73@users.noreply.github.com>
2025-01-11 20:59:07 +0530

    Add files via upload

---
+++ INTEGER AND FLOAT.py
@@ -0,0 +1,40 @@
+'''
+#INTEGER AND FLOAT
+
+#TYPE OF NUMBER
+
+num = 3
+print (type(num))
+
+num = 3.14
+print (type(num))
+
+#ARITHEMETIC OPERATORS (+,-,*,%,/,//)
+
+num =4
+
+num*=20
+
+print(num)
+
+#ABSOLUTE VALUE
+
+print(abs(-3))
+
+#ROUND OFF
+
+value=3.555
+print(round(value))
+
+#COMPARISON OPERATOR [( BOOLEAN)==,!=,>=,<=,<,>]
+
+num_1=3
+num_2=4
+
+print(num_1 == num_2)
+
+#INTEGER TYPE CASTING
+
+NUM ='12'
+NUM =int(NUM)
+,,,
